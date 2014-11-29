4e4df2b209825f3b66dfe1e95ff4bd54ec40fc2b
marcwebbie <marcwebbie@gmail.com>
2014-11-29 02:23:55 -0300

    Updating structure to html5 boilerplate

---
+++ js/main.js
@@ -0,0 +1,111 @@
+
+function formatSeconds(seconds) {
+  var date = new Date(1970,0,1);
+  date.setSeconds(seconds);
+  return date.toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1");
+}
+
+function formatSrtTime(seconds) {
+  var msecs = (seconds % 1).toString().split('.')[1];
+  msecs = msecs.substring(0, 3);
+  var formatedTime = formatSeconds(seconds);
+  var srtTime = formatedTime + "," + msecs;
+  return srtTime;
+}
+
+function buildSubtitle(id, startTime, endTime, text){
+  var subtitle = new Object();
+  subtitle.id = id;
+  subtitle.startTime = startTime;
+  subtitle.endTime = endTime;
+  subtitle.text = text;
+  return subtitle;
+}
+
+function getNextId(subtitlesArray){
+  if(subtitlesArray.length > 0){
+    var lastId = subtitlesArray[subtitlesArray.length - 1]["id"];
+    return lastId + 1;
+  }
+  else{
+    return 1;
+  }
+}
+
+function formatSrtSubtitleString(subtitle){
+  // numéro du sous-titre
+  // Temps de départ --> temps de fin
+  // Texte du sous-titre (peut être sur plusieurs lignes)
+  // [Ligne vide]
+
+  var subtitleString = "";
+  subtitleString += subtitle.id.toString();
+  subtitleString += "\n";
+  subtitleString += formatSrtTime(subtitle.startTime);
+  subtitleString += " --> ";
+  subtitleString += formatSrtTime(subtitle.endTime);
+  subtitleString += "\n";
+  subtitleString += subtitle.text;
+  subtitleString += "\n";
+  subtitleString += "\n";
+
+  return subtitleString;
+}
+
+var subtitles = []
+var currentSubStartTime = "";
+var currentSubEndTime = "";
+var currentSubText = "";
+
+$( "#start-button" ).click(function() {
+  // set start subtime
+  currentSubStartTime = player.getCurrentTime();
+
+  // set color button sucess
+  $( "#start-button" ).addClass("button-sucess");
+
+  // alert( "Handler for start-button .click() called." );
+});
+
+$( "#end-button" ).click(function() {
+  // set end subs time
+  currentSubEndTime = player.getCurrentTime();
+
+  // pause video
+  player.pauseVideo();
+
+  // show sub-entry-form
+  $( "#sub-entry-form" ).show();
+
+  // alert( "Handler for end-button .click() called." );
+});
+
+$( "#sub-text" ).keyup(function() {
+  currentSubText = $("#sub-text").val();
+  // alert( "Handler for .keyup() called." );
+});
+
+$( "#save-button" ).click(function() {
+  // append subtitle object to subtitles array
+  sub = buildSubtitle(
+    getNextId(subtitles),
+    currentSubStartTime,
+    currentSubEndTime,
+    currentSubText
+  );
+  subtitles.push(sub);
+
+  // append subtitle to subs-result
+  subtitleString = formatSrtSubtitleString(sub);
+  $( "#sub-result" ).val($( "#sub-result" ).val() + subtitleString);
+
+  console.log(subtitleString);
+
+  // clear sub-text
+  $( "#sub-text" ).val("");
+
+  // hide sub-entry-form
+  $( "#sub-entry-form" ).hide();
+
+  // alert( "Handler for save-button .click() called." );
+});
---
+++ js/yt.js
@@ -0,0 +1,32 @@
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '390',
+    width: '640',
+    videoId: 'M7lc1UVf-VE',
+    events: {
+      'onReady': onPlayerReady
+      // 'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+function setSrtDuration() {
+  var dur = formatSrtTime(player.getCurrentTime());
+  //console.log("setSrtDuration: " + dur);
+  $('#time').html(dur);
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+  // stuff to do when player is ready
+}
